05398c85ce7fa490d1b7ec64f3ad575d8353eefb
Faiz Khalifah <faiz_doc@outlook.com>
2024-11-22 21:34:02 +0700

    feat: auth and owner data class

---
+++ app/src/main/java/com/praktikum/honeypot/Data/Owner.kt
@@ -0,0 +1,11 @@
+package com.praktikum.honeypot.Data
+
+data class Owner(
+    val owner_id: Int,
+    val username: String,
+    val password: String,
+    val full_name: String,
+    val contact: String,
+    val createdAt: String,
+    val updatedAt: String
+)
