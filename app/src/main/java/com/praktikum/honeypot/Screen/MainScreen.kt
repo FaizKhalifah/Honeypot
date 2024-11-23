3a6d46f3883486ae2729fc25d278f58fc27ab7e8
Faiz Khalifah <faiz_doc@outlook.com>
2024-11-23 16:39:43 +0700

    feat:main pages navigation bar

--- app/src/main/java/com/praktikum/honeypot/Screen/MainScreen.kt
+++ app/src/main/java/com/praktikum/honeypot/Screen/MainScreen.kt
@@ -1,11 +1,31 @@
 package com.praktikum.honeypot.Screen
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.praktikum.honeypot.Navigation.BottomNavigationBar
 
 @Composable
-fun MainScreen(){
-    Text(
-        text = "Ini halaman main"
-    )
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController) }
+    ) { paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = "home",
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            composable("home") { HomeScreen() }
+            composable("product") { ProductScreen() }
+            composable("report") { ReportScreen() }
+            composable("partner") { PartnerScreen() }
+            composable("profile") { ProfileScreen() }
+        }
+    }
 }
