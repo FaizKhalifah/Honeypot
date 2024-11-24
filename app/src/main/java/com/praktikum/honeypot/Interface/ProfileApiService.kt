d4b2b5b09928d1b79587bc01f9a83cd78cdb5ee1
Ananda Ravi Kuntadi <anandaravik@gmail.com>
2024-11-24 16:25:04 +0700

    feat(profile): add profile section with password update functionality and improved API integration

--- app/src/main/java/com/praktikum/honeypot/Interface/ProfileApiService.kt
+++ app/src/main/java/com/praktikum/honeypot/Interface/ProfileApiService.kt
@@ -6,10 +6,17 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.PUT
 
+data class ApiResponse(
+    val message: String // Add this to capture the API response message
+)
+
 interface ProfileApiService {
     @GET("api/owner/profile")
     fun getProfile(): Call<Owner>
 
     @PUT("api/owner/profile")
     fun updateProfile(@Body updateData: Map<String, String>): Call<Void>
+
+    @PUT("api/owner/change-password")
+    fun changePassword(@Body passwordData: Map<String, String>): Call<ApiResponse>
 }
